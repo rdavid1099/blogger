341c4f21271a33744904178df9c1fc72fd804134
Ryan Workman <rdavid1099@gmail.com>
2016-08-31 16:48:20 -0600

    Implement full CRUD functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,46 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find( params[:id] )
   end
+
+  def create
+    @article = Article.new( article_params )
+    if @article.save
+      flash.notice = "Article Saved!"
+      redirect_to article_path(@article)
+    else
+      render :new
+    end
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find( params[:id] )
+  end
+
+  def update
+    @article = Article.find( params[:id] )
+    @article.update( article_params )
+    if @article.save
+      flash.notice = "Article '#{@article.title}' Updated"
+      redirect_to article_path(@article)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article = Article.find( params[:id] )
+    @article.destroy
+    flash.notice = "Article Deleted!"
+    redirect_to articles_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
