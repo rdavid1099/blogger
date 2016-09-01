71782410bdbbed42c5d0e6be8318a3e224756668
Ryan Workman <rdavid1099@gmail.com>
2016-08-31 18:25:12 -0600

    Add tags and tag navigation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160831225015) do
+ActiveRecord::Schema.define(version: 20160831234726) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -26,6 +26,19 @@ ActiveRecord::Schema.define(version: 20160831225015) do
     t.integer  "article_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "taggings", force: :cascade do |t|
+    t.integer  "tag_id"
+    t.integer  "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
